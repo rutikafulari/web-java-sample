39873a1873afcdc9af462d2c6a50f33be59ea905
rushi <rushi@localhost>
2026-05-03 13:41:22 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java! This is a simple main class.");
+        System.out.println("You can compile with: javac Main.java");
+        System.out.println("And run with: java Main");
+    }
+}
